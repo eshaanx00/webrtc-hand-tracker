2505210a38e53bc1cae5cd6f203d15d476550921
eshaanx00 <69445437+eshaanx00@users.noreply.github.com>
2021-12-24 15:26:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
 import streamlit as st
 import cv2
 from streamlit_webrtc import VideoTransformerBase, webrtc_streamer
-import HandTracker as htm
+import mediapipe as mp
 
-detector = htm.HandDetector()
+mpHands = mp.solutions.hands
+Hands = mpHands.Hands(min_detection_confidence=0.75,min_tracking_confidence=0.75,max_num_hands=2)
+mpDraw = mp.solutions.drawing_utils
+
+def drawHands(img):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        results = Hands.process(imgRGB)
+        if results.multi_hand_landmarks:
+                for handLms in results.multi_hand_landmarks:
+                        mpDraw.draw_landmarks(img,handLms,mpHands.HAND_CONNECTIONS)
+        return img
+
 
 class VideoTransformer(VideoTransformerBase):
     def transform(self, frame):
         img = frame.to_ndarray(format="bgr24")
 
-        img = detector.drawHands(img=img)
+        img = drawHands(img=img)
+        
 
         return img
 
