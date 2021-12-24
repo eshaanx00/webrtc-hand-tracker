3a5843e49fcc91adb08f43e67494fddc9ac38cf3
eshaanx00 <69445437+eshaanx00@users.noreply.github.com>
2021-12-24 16:06:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,29 @@
+import streamlit as st
 import cv2
 from streamlit_webrtc import VideoTransformerBase, webrtc_streamer
+import mediapipe as mp
 
+mpHands = mp.solutions.hands
+Hands = mpHands.Hands(min_detection_confidence=0.75,min_tracking_confidence=0.75,max_num_hands=2)
+mpDraw = mp.solutions.drawing_utils
 
-class VideoTransformer(VideoTransformerBase):
-    def transform(self, frame):
-        img = frame.to_ndarray(format="bgr24")
-
-        img = cv2.cvtColor(cv2.Canny(img, 100, 200), cv2.COLOR_GRAY2BGR)
-
+def drawHands(img):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        results = Hands.process(imgRGB)
+        if results.multi_hand_landmarks:
+                for handLms in results.multi_hand_landmarks:
+                        mpDraw.draw_landmarks(img,handLms,mpHands.HAND_CONNECTIONS)
         return img
 
+def main():
+    class VideoTransformer(VideoTransformerBase):
+        def transform(self, frame):
+            img = frame.to_ndarray(format="bgr24")
+            img = drawHands(img=img)
+            return img
 
-webrtc_streamer(key="example", video_transformer_factory=VideoTransformer)
+
+    ctx = webrtc_streamer(key="example", video_transformer_factory=VideoTransformer,media_stream_constraints={"video": True, "audio": False})
+
+if __name__ == "__main__":
+    main()
